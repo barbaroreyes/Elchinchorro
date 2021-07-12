63841f731ecc4dae1ac8a4fa611c88ca0a55aac1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-12 02:39:32 -0500

    added-auth

---
+++ pages/create-product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {withAuthenticator} from '@aws-amplify/ui-react'
+const CreateProduct = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default withAuthenticator (CreateProduct);
