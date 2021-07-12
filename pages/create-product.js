f910d548cfab77a3cb64721f8c589613300ffba1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-07-12 02:57:27 -0500

    adde-state

--- pages/create-product.js
+++ pages/create-product.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react'
 const CreateProduct = () => {
+    const [name, setName]= useState('')
+    const [image, setImage]= useState('')
   return (
     <div>
-      <h1>Hello</h1>
-      <AmplifySignOut/>
+    <form>
+        <h2>Create product</h2>
+        <label htmlFor='name'>Product Name</label>
+        <input type='text' id='name' onChange ={e=> setName(e.target.value)}/>
+        <input type='file' id='image' onChange ={e=> setImage(e.target.files[0])}/>
+        <input type='submit' value='create'/>
+
+    </form>
+      {/* <AmplifySignOut/> */}
     </div>
   );
 }
